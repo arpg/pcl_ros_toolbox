b948fffcb9ad5a7276a34893281cbf58988ea823
Mike M <mikemiles19@gmail.com>
2021-01-13 13:36:19 -0700

    implemented 2-msg policy and added params in approx_time

--- src/approx_time_concatenate_server.cpp
+++ src/approx_time_concatenate_server.cpp
@@ -11,12 +11,17 @@
 
 #include <pcl_ros/point_cloud.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/transforms.h>
 
 
 #define MAX_NUM_CHANNELS 9
 
 namespace pcl_ros_toolbox
 {
+
+typedef message_filters::sync_policies::ApproximateTime
+        <sensor_msgs::PointCloud2,
+        sensor_msgs::PointCloud2> approx_time_sync_policy_2;
 
 typedef message_filters::sync_policies::ApproximateTime
         <sensor_msgs::PointCloud2,
@@ -34,6 +39,8 @@ typedef message_filters::sync_policies::ApproximateTime
         sensor_msgs::PointCloud2,
         sensor_msgs::PointCloud2> approx_time_sync_policy_9;
 
+typedef approx_time_sync_policy_2 approx_time_sync_policy;
+
 class ApproxTimeConcatenateServer : public nodelet::Nodelet
 {
 public:
@@ -41,11 +48,19 @@ public:
 
   inline virtual void onInit()
   {
+    ROS_INFO("Initializing...");
+
     // nh_ = getNodeHandle();
     // private_nh_ = getPrivateNodeHandle();
 
-    approx_time_sync_policy_3 sync_policy(5);
-    sync_policy.setMaxIntervalDuration(ros::Duration(0.05));
+    // getPrivateNodeHandle().param("num_inputs", num_inputs_, int(2));
+    getPrivateNodeHandle().param("common_frame_id", common_frame_id_, std::string("world"));
+    getPrivateNodeHandle().param("min_rate", min_rate_, double(4));
+    getPrivateNodeHandle().param("sync_queue_size", sync_queue_size_, int(5));
+    getPrivateNodeHandle().param("sub_queue_size", sub_queue_size_, int(5));
+
+    approx_time_sync_policy sync_policy(sync_queue_size_);
+    sync_policy.setMaxIntervalDuration(ros::Duration(1.0/min_rate_));
     // sync_policy.setInterMessageLowerBound(0, ros::Duration(0.05));
     // sync_policy.setInterMessageLowerBound(1, ros::Duration(0.05));
     // sync_policy.setInterMessageLowerBound(2, ros::Duration(0.05));
@@ -53,24 +68,71 @@ public:
     pub_ = getPrivateNodeHandle().advertise<sensor_msgs::PointCloud2>("output", 1);
 
     subs_.clear();
-    for (uint i=0; i<3; i++)
+    for (uint i=0; i<2; i++)
     {
-      subs_.push_back(new message_filters::Subscriber<sensor_msgs::PointCloud2>(getPrivateNodeHandle(), "input_"+std::to_string(i), 5));
+      subs_.push_back(new message_filters::Subscriber<sensor_msgs::PointCloud2>(getPrivateNodeHandle(), "input_"+std::to_string(i), sub_queue_size_));
     }
+
     uint i=0;
-    sync_ = new message_filters::Synchronizer <approx_time_sync_policy_3>(static_cast<const approx_time_sync_policy_3 &>(sync_policy)
-      ,*(subs_[i++])
+    sync_ = new message_filters::Synchronizer <approx_time_sync_policy>(static_cast<const approx_time_sync_policy &>(sync_policy)
       ,*(subs_[i++])
       ,*(subs_[i++]) );
     // sync.registerCallback(boost::bind(&ApproxTimeConcatenateServer::callback, this));
     // sync.registerCallback(boost::bind(&ApproxTimeConcatenateServer::callback, this, _1, _2, _3, _4, _5, _6, _7, _8, _9));
-    sync_->registerCallback(boost::bind(&ApproxTimeConcatenateServer::callback, this, _1, _2, _3));
+    sync_->registerCallback(boost::bind(&ApproxTimeConcatenateServer::callback, this, _1, _2));
 
   };
 
+  inline bool getTransformFromTree(std::string parent_frame_id, std::string child_frame_id, tf::StampedTransform& tform_msg, ros::Time stamp=ros::Time(0), double timeout=0.5)
+  {
+    // tf::StampedTransform tform_msg;
+    static tf::TransformListener listener;
+
+    if (parent_frame_id == child_frame_id)
+    {
+      ROS_WARN("No need to get transform for frame %s. Assuming Identity.", parent_frame_id.c_str());
+      tform_msg.setIdentity();
+      return true;
+    }
+
+    try
+    {
+      listener.waitForTransform(parent_frame_id, child_frame_id, ros::Time(0), ros::Duration(timeout));
+      listener.lookupTransform(parent_frame_id, child_frame_id, stamp, tform_msg);
+    }
+    catch (tf::TransformException ex)
+    {
+      // ROS_ERROR("No link between %s and %s. Assuming Identity.", parent_frame_id.c_str(), child_frame_id.c_str());
+      ROS_ERROR("%s",ex.what());
+      tform_msg.setIdentity();
+      return false;
+    }
+
+    // Eigen::Affine3d tform;
+    // tf::transformTFToEigen(tf::Transform(tform_msg),tform);
+    // (*tform_mat) = tform.matrix();
+    return true;
+  }
+
+  inline bool transformAndConcatCloud(pcl::PointCloud<pcl::PointXYZ> partial_cloud, pcl::PointCloud<pcl::PointXYZ>& whole_cloud)
+  {
+    tf::StampedTransform tform_msg;
+    // if(!getTransformFromTree(common_frame_id_, partial_cloud.header.frame_id, tform_msg, ros::Time().fromNSec(partial_cloud.header.stamp*1000) ))
+    if(!getTransformFromTree(whole_cloud.header.frame_id, partial_cloud.header.frame_id, tform_msg ))
+    {
+      // ROS_WARN("Failed to get transform from tf tree.");
+      return false;
+    }
+    pcl_ros::transformPointCloud (partial_cloud, partial_cloud, tform_msg);
+    whole_cloud += partial_cloud;
+    whole_cloud.header.stamp = partial_cloud.header.stamp;
+    
+    return true;
+  }
+
   inline void callback(const sensor_msgs::PointCloud2ConstPtr& input0
                 , const sensor_msgs::PointCloud2ConstPtr& input1
-                , const sensor_msgs::PointCloud2ConstPtr& input2
+                // , const sensor_msgs::PointCloud2ConstPtr& input2
                 // , const sensor_msgs::PointCloud2ConstPtr& input3
                 // , const sensor_msgs::PointCloud2ConstPtr& input4
                 // , const sensor_msgs::PointCloud2ConstPtr& input5
@@ -81,9 +143,9 @@ public:
   {
     ROS_INFO("ApproxTimeConcatenateServer called.");
 
-    ROS_INFO("Received input0 with size %d", input0->width);
-    ROS_INFO("Received input1 with size %d", input1->width);
-    ROS_INFO("Received input2 with size %d", input2->width);
+    ROS_INFO("Received input0 with size %d frame %s stamp %f", input0->width, input0->header.frame_id.c_str(), input0->header.stamp.toSec());
+    ROS_INFO("Received input1 with size %d frame %s stamp %f", input1->width, input1->header.frame_id.c_str(), input1->header.stamp.toSec());
+    // ROS_INFO("Received input2 with size %d", input2->width);
     // ROS_INFO("Received input3 with size %d", input3->width);
     // ROS_INFO("Received input4 with size %d", input4->width);
     // ROS_INFO("Received input5 with size %d", input5->width);
@@ -91,20 +153,25 @@ public:
     // ROS_INFO("Received input7 with size %d", input7->width);
     // ROS_INFO("Received input8 with size %d", input8->width);
 
-    pcl::PointCloud<pcl::PointXYZ> cloud0, cloud1, cloud2;//, cloud0, cloud0, cloud0, cloud0, 
+    pcl::PointCloud<pcl::PointXYZ> whole_cloud, cloud0, cloud1;//, cloud2;//, cloud0, cloud0, cloud0, cloud0, 
+    whole_cloud.header.frame_id = common_frame_id_;
    
 		pcl::fromROSMsg(*input0, cloud0);
 		pcl::fromROSMsg(*input1, cloud1);
-		pcl::fromROSMsg(*input2, cloud2);
+		// pcl::fromROSMsg(*input2, cloud2);
 
-    pcl::PointCloud<pcl::PointXYZ> concat_cloud = cloud0;
-    concat_cloud += cloud1;
-    concat_cloud += cloud2;
+    if (!transformAndConcatCloud(cloud0, whole_cloud)) return;
+    if (!transformAndConcatCloud(cloud1, whole_cloud)) return;
 
+    if (whole_cloud.size()==0)
+    {
+      ROS_WARN("Empty cloud. Skipping...");
+      return;
+    }
     sensor_msgs::PointCloud2 output;
-    pcl::toROSMsg(concat_cloud, output);
+    pcl::toROSMsg(whole_cloud, output);
 
-    ROS_INFO("Publishing concatenated cloud with size %d", output.width);
+    ROS_INFO("Publishing concatenated cloud with size %d frame %s", output.width, output.header.frame_id.c_str());
 
     pub_.publish(output);
     ros::spinOnce();
@@ -113,9 +180,15 @@ public:
 private:
   // ros::NodeHandle& nh_;
   // ros::NodeHandle& private_nh_;
-  message_filters::Synchronizer <approx_time_sync_policy_3>* sync_;
+  message_filters::Synchronizer <approx_time_sync_policy>* sync_;
   std::vector<message_filters::Subscriber<sensor_msgs::PointCloud2>*> subs_;
   ros::Publisher pub_;
+
+  // uint num_inputs_;
+  std::string common_frame_id_;
+  double min_rate_;
+  int sync_queue_size_;
+  int sub_queue_size_;
 };
 
 } // namespace pcl_ros_toolbox
