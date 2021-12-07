10e31db785525acf62bf80d0149ab6211fc574e0
Mike Miles <mikemiles19@gmail.com>
2021-12-07 00:03:36 -0700

    in process of convering GTG to bagfiles

--- include/pcl_ros_toolbox/groundtruth_trajectory_generator.h
+++ include/pcl_ros_toolbox/groundtruth_trajectory_generator.h
@@ -4,20 +4,17 @@
 #include <tf/transform_listener.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <goicp/EstimateTransform.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
 
 class GroundtruthTrajectoryGenerator
 {
 public:
 	GroundtruthTrajectoryGenerator(ros::NodeHandle& );
-	void cloudCallback(const sensor_msgs::PointCloud2::ConstPtr& );
-	void gtCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& );
-	void processCloud();
+	void processBag();
 private:
 	ros::NodeHandle nh;
-	std::string bagfile, cloud_topic, gt_cloud_topic, gt_path_topic;
+	std::string input_bagfile, output_bagfile, cloud_topic, gt_input_bagfile, gt_path_topic;
 	ros::ServiceClient icp_client;
-    ros::Subscriber cloud_sub, gt_cloud_sub;
-    ros::Publisher gt_path_pub;
     tf::TransformListener tf_listener;
-	sensor_msgs::PointCloud2::ConstPtr gt_cloud_msg, cloud_msg;
 };
--- src/groundtruth_trajectory_generator.cpp
+++ src/groundtruth_trajectory_generator.cpp
@@ -2,40 +2,28 @@
 
 GroundtruthTrajectoryGenerator::GroundtruthTrajectoryGenerator(ros::NodeHandle& nh_) : nh(nh_)
 {
-    nh.param<std::string>("bagfile", bagfile, std::string("darpa_final.bag").c_str());
+    nh.param<std::string>("input_bagfile", input_bagfile, std::string("darpa_final.bag").c_str());
     nh.param<std::string>("cloud_topic", cloud_topic, std::string("horiz/os_cloud_node/points").c_str());
-    nh.param<std::string>("gt_cloud_topic", gt_cloud_topic, std::string("/darpa_final_cloud").c_str());
+    nh.param<std::string>("gt_input_bagfile", gt_input_bagfile, std::string("/darpa_final_gt_cloud").c_str());
     nh.param<std::string>("gt_path_topic", gt_path_topic, std::string("/darpa_final_path").c_str());
+    nh.param<std::string>("output_bagfile", output_bagfile, std::string("darpa_final_path.bag").c_str());
 
     icp_client = nh.serviceClient<goicp::EstimateTransform>("/go_icp_server/estimate_transform");
 
-    cloud_sub = nh.subscribe(cloud_topic, 1, &GroundtruthTrajectoryGenerator::cloudCallback, this);
-    gt_cloud_sub = nh.subscribe(gt_cloud_topic, 1, &GroundtruthTrajectoryGenerator::gtCloudCallback, this);
-    
-    gt_path_pub = nh.advertise<nav_msgs::Path>(gt_path_topic, 1, true);    
+    processBag();
 }
 
-void GroundtruthTrajectoryGenerator::cloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+void GroundtruthTrajectoryGenerator::processBag()
 {
-    cloud_msg = msg;
-    processCloud();
-}
+    rosbag::Bag inbag;
+    inbag.open(input_bagfile, rosbag::bagmode::Read);
 
-void GroundtruthTrajectoryGenerator::gtCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
-{
-    gt_cloud_msg = msg;
-}
-
-void GroundtruthTrajectoryGenerator::processCloud()
-{
-    if (!cloud_msg)
+    for(rosbag::MessageInstance const m: rosbag::View(inbag))
     {
-        ROS_WARN("Cloud has not been received. Cannot process cloud. Skipping...");
-        return;
+        sensor_msgs::PointCloud2::ConstPtr cloud_msg = m.instantiate<sensor_msgs::PointCloud2>();
+        if (cloud_msg != NULL)
+            ROS_INFO("on cloud");
     }
-    if (!gt_cloud_msg)
-    {
-        ROS_WARN("Groundtruth cloud has not been received. Cannot process cloud. Skipping...");
-        return;
-    }
+
+    inbag.close();
 }
